6e7c65bf3fe0e7c5d81a51ba752e520fd4e97fcc
torrence <tlstratton@gmail.com>
2020-02-10 18:12:51 -0800

    Some edits

--- example.tf
+++ example.tf
@@ -1,9 +1,28 @@
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = var.region
+}
+
+# New resource for the S3 bucket our application will use.
+resource "aws_s3_bucket" "example" {
+  bucket = "crabbytabby2020"
+  acl    = "private"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
+    ami           = "ami-038e6b679579b4ec1"
+    instance_type = "t2.micro"
+
+    # Tells Terraform that this EC2 instance must be created only after the
+    # S3 bucket has been created.
+    depends_on = [aws_s3_bucket.example]
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
 }
